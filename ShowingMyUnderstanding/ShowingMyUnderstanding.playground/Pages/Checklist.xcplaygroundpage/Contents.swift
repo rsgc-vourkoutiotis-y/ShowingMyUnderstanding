b3aecfb8c9f99a24f1d58889f6c1d75c912f7bca
Russell Gordon <rgordon@rsgc.on.ca>
2017-02-23 09:50:10 -0500

    Fixed due date

--- ShowingMyUnderstanding/ShowingMyUnderstanding.playground/Pages/Checklist.xcplaygroundpage/Contents.swift
+++ ShowingMyUnderstanding/ShowingMyUnderstanding.playground/Pages/Checklist.xcplaygroundpage/Contents.swift
@@ -13,6 +13,6 @@
  * where you are using variables?
  * where you are using assignment statements?
  
- **Due date:** The assignment is due, without exception, by the start of our next class (Monday, March 27, 2017).
+ **Due date:** The assignment is due, without exception, by the start of our next class (Monday, February 27, 2017).
   
  */
