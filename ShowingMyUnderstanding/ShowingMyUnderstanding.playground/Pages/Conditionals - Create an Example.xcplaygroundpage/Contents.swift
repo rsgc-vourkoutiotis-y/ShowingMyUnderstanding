2801ef12092c09d4bafcb6cbefc4bd69224bb3fb
Student <student@Yanni-Vourkoutiotis.local>
2017-02-25 18:25:30 -0500

    Finished conditional practice but playground does not work. Code is fine.

--- ShowingMyUnderstanding/ShowingMyUnderstanding.playground/Pages/Conditionals - Create an Example.xcplaygroundpage/Contents.swift
+++ ShowingMyUnderstanding/ShowingMyUnderstanding.playground/Pages/Conditionals - Create an Example.xcplaygroundpage/Contents.swift
@@ -19,6 +19,33 @@ import PlaygroundSupport
 // Create canvas
 let canvas = Canvas(width: 300, height: 300)
 
-// Replace this command and add your code below...
+// If number = 0, then draw line to (100,150)
+// If number = 1, then draw line to (200,150)
 
+// Generate a random number
+var number = random(from: 0, toButNotIncluding: 2)
+
+
+if number == 0 {
+    
+    canvas.drawLine(fromX: 0, fromY: 150, toX: 100, toY: 150)
+}
+    
+    
+else {
+    
+    canvas.drawLine(fromX: 0, fromY: 150, toX: 200, toY: 150)
+}
+
+
+
+
+
+
+
+
+/*:
+ ## Template code
+ The code below is necessary to see results in the Assistant Editor at right. Please do not remove.
+ */
 PlaygroundPage.current.liveView = canvas.imageView
